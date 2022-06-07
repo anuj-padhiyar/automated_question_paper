888605ce176b7e716fb7da3ff9feaf973e4b17ce
anuj padhiyar <padhiyaranuj742000@gmail.com>
2022-06-07 12:18:33 +0530

    7th Jun

--- crowd_sourcing_model/user/models.py
+++ crowd_sourcing_model/user/models.py
@@ -1,3 +1,23 @@
+from pyexpat import model
+from re import T
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    email = models.CharField(max_length=256,db_index=True,unique=True,default='')
+
+    def __unicode__(self):
+        return u'{}-{}'.format(self.email,self.get_user_name())
+
+    def to_json(self):
+        return {
+            'id' : self.id,
+            'name' : self.name,
+            'email' : self.email
+        }
+
+    def get_user_name(self):
+        return self.first_name + ' ' + self.last_name
+    
--- crowd_sourcing_model/user/views.py
+++ crowd_sourcing_model/user/views.py
@@ -1,3 +1,13 @@
+from __future__ import unicode_literals
+
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.generic.base import View
+
 
 # Create your views here.
+def user(request):
+    return JsonResponse({'code': 200, 'msg': 'Your Balance Amount is: 2,00,000 Rs/-'})
+
+def login(request):
+    return render(request, 'user/login.html',{})
